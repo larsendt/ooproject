87fb00c2fa5d7a7fb1332e7071b9212e5941e61d
Dane Larsen <dane.t.larsen@gmail.com>
2012-12-02 17:38:06 -0700

    fixed LRU to use atime instead of mtime

--- server/filecache.py
+++ server/filecache.py
@@ -1,4 +1,8 @@
-import os, json, hashlib
+import os
+import json
+import hashlib
+import time
+import util
 
 
 def get_hash(item):
@@ -15,18 +19,6 @@ def get_size(start_path = '.'):
             total_size += os.path.getsize(fp)
     return total_size
 
-# drop least recently used cache items until the total 
-# size of the dir is <= max_size
-def drop_lru(directory, max_size):
-    while get_size(directory) > max_size:
-        oldest_file = None
-
-        for f in os.listdir(directory):
-            if (oldest_file is None) or (os.path.getmtime(os.path.join(directory, oldest_file)) > os.path.getmtime(os.path.join(directory, f))):
-                oldest_file = f
-
-        print "dropped %s from the cache, total size is now %d KB" % (os.path.join(directory, oldest_file), get_size(directory)/1000.0)
-        os.remove(os.path.join(directory, oldest_file))
 
 class Cache(object):
     """ On-disk key-value store """
@@ -35,9 +27,12 @@ class Cache(object):
         self.directory = directory
         self.name = cache_name
         self.max_size = max_cache_size
+        print "filecache max size: %s" % util.SI_byte_string(self.max_size)
 
         if not os.path.exists(self.directory):
             os.mkdir(self.directory)
+
+        print "filecache current size: %s" % util.SI_byte_string(get_size(self.directory))
 
     def __contains__(self, key):
         k = get_hash(key)
@@ -60,14 +55,14 @@ class Cache(object):
             obj = json.loads(f.read())
             f.close()
             
-            # there probably will only ever be one chunk per file,
-            # but do this incase of collisions (the price of hashing)
-            return obj[get_json_key(key)]
+            # update the atime for the LRU
+            obj[get_json_key(key)]["atime"] = time.time()
+            return obj[get_json_key(key)]["value"]
         else:
             raise KeyError("Cache did not have value for key '%s'", key)
 
     def __setitem__(self, key, value):
-        drop_lru(self.directory, self.max_size)
+        self.drop_lru()
 
         k = get_hash(key)
         fname = os.path.join(self.directory, k)
@@ -80,10 +75,38 @@ class Cache(object):
             obj = json.loads(f.read())
             f.close()
 
-        obj[get_json_key(key)] = value
+        # set the access time for LRU
+        obj[get_json_key(key)] = {"atime":time.time(), "value":value}
 
         f = open(fname, "w")
         f.write(json.dumps(obj))
         f.close()
 
         
+    # drop least recently used cache items until the total 
+    # size of the dir is <= max_size
+    def drop_lru(self):
+        print "cache size:", util.SI_byte_string(get_size(self.directory))
+        while get_size(self.directory) > self.max_size:
+            oldest_file = None
+            oldest_time = None
+            oldest_key = None
+            
+
+            for f in os.listdir(self.directory):
+                path = os.path.join(self.directory, f)
+                f = open(path, "r")
+                obj = json.loads(f.read())
+                f.close()
+
+                if (oldest_time is None) or (obj[obj.keys()[0]]["atime"] < oldest_time):
+                    oldest_time = obj[obj.keys()[0]]["atime"]
+                    oldest_file = path
+                    oldest_key = obj.keys()[0]
+
+            os.remove(oldest_file)
+            print "dropped %s (%s), cache is now %s" % (oldest_file, oldest_key, util.SI_byte_string(get_size(self.directory)))
+
+
+
+
