7ae9941bd7afe6ce8ff0c20a7f5a5f1069af4c37
Dane Larsen <dane.t.larsen@gmail.com>
2012-12-11 23:05:38 -0700

    terrain type is selected via dropdown

--- android/OpenGLES20Complete/src/com/larsendt/ObjectOriented/SetupActivity.java
+++ android/OpenGLES20Complete/src/com/larsendt/ObjectOriented/SetupActivity.java
@@ -5,8 +5,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 
+import android.widget.Spinner;
 import com.larsendt.ObjectOriented.R;
 
 public class SetupActivity extends Activity {
@@ -15,6 +17,10 @@ public class SetupActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_init);
+        Spinner terrainSpinner = (Spinner) findViewById(R.id.terrainSpinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.terrain_type_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        terrainSpinner.setAdapter(adapter);
     }
 
     @Override
@@ -28,11 +34,11 @@ public class SetupActivity extends Activity {
     	EditText editText = (EditText) findViewById(R.id.editText1);
     	String message = editText.getText().toString();//"http://larsendt.com:1234";
     	intent.putExtra("com.larsendt.ObjectOriented.serverName", message);
-    	
-    	editText = (EditText) findViewById(R.id.editText2);
-    	String message2 = editText.getText().toString();
-    	intent.putExtra("com.larsendt.ObjectOriented.terrainType",message2);
-    	
+
+        Spinner terrainSpinner = (Spinner) findViewById(R.id.terrainSpinner);
+        String message2 = terrainSpinner.getSelectedItem().toString();
+        intent.putExtra("com.larsendt.ObjectOriented.terrainType",message2);
+
     	startActivity(intent);
     }
 }
